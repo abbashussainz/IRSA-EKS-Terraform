8a3f81eda147a6bd30298daa3704444d2a6538ab
abbashussainz <abbashussain.x@gmail.com>
2023-04-09 01:05:59 +0530

    vpc configuration files addes

--- Manifest Files/vpc-variables.tf
+++ Manifest Files/vpc-variables.tf
@@ -3,63 +3,63 @@
 # VPC CIDR Block
 variable "vpc_cidr_block" {
   description = "VPC CIDR Block"
-  type = string 
-  default = "10.0.0.0/16"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 
 
 variable "vpc_name" {
   default = "IRSA-EKS-VPC"
-  type = string
+  type    = string
 
 }
 
 variable "vpc_public_subnets" {
   description = "VPC Public Subnets"
-  type = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 
 variable "vpc_private_subnets" {
   description = "VPC Private Subnets"
-  type = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 
 variable "vpc_database_subnets" {
   description = "VPC Database Subnets"
-  type = list(string)
-  default = ["10.0.151.0/24", "10.0.152.0/24"]
+  type        = list(string)
+  default     = ["10.0.151.0/24", "10.0.152.0/24"]
 }
 
 
 variable "vpc_create_database_subnet_group" {
   description = "VPC Create Database Subnet Group"
-  type = bool
-  default = true 
+  type        = bool
+  default     = true
 }
 
 variable "vpc_create_database_subnet_route_table" {
   description = "VPC Create Database Subnet Route Table"
-  type = bool
-  default = true   
+  type        = bool
+  default     = true
 }
 
 
 variable "vpc_enable_nat_gateway" {
   description = "Enable NAT Gateways for Private Subnets Outbound Communication"
-  type = bool
-  default = true  
+  type        = bool
+  default     = true
 }
 
 
 variable "vpc_single_nat_gateway" {
   description = "Enable only single NAT Gateway in one Availability Zone to save costs during our demos"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 
--- Manifest Files/vpc.tf
+++ Manifest Files/vpc.tf
@@ -1,21 +1,16 @@
-data "aws_availability_zone" "available" {
-
-}
-
 module "vpc" {
-  source  = "terrafrom-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "3.11.0"
-
   name           = var.vpc_name
   cidr           = var.vpc_cidr_block
-  azs            = data.aws_availability_zone.available.names
+  azs            = ["ap-south-1a","ap-south-1b"]
   public_subnets = var.vpc_public_subnets
-  private_sunets = var.vpc_private_subnets
+  private_subnets = var.vpc_private_subnets
 
   database_subnets                   = var.vpc_database_subnets
   create_database_subnet_group       = var.vpc_create_database_subnet_group
   create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
- 
+
 
   enable_nat_gateway = var.vpc_enable_nat_gateway
   single_nat_gateway = var.vpc_single_nat_gateway
